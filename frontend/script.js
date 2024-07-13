ef5e3e33d1a786be1f0ce0c24841cc2cf7a0ea7e
nanadotam <nanaamoako202@gmail.com>
2024-07-13 01:04:51 +0000

    Improvements

--- frontend/script.js
+++ frontend/script.js
@@ -46,3 +46,46 @@
 // resetButton.addEventListener('click', resetTimer);
 
 // updateTimeDisplay();
+
+
+// Variables Initialization
+let workTitle = document.getElementById('Work');
+let breakTitle = document.getElementById('Break');
+
+let workTime = 25;
+let breakTime = 5;
+
+let seconds = '00';
+
+// display
+window.onload = () => {
+    document.getElementById('minutes').innerHTML = workTime;
+    document.getElementById('seconds').innerHTML = seconds;
+
+    workTitle.classList.add('active');
+}
+
+// timer (start)
+function start() {
+    // change the time
+    seconds = 59;
+
+    let workMinutes = workTime - 1;
+    let breakTime = breakTime - 1;
+
+    breakCount = 0;
+
+    //countdown
+    let timerFunction = () => {
+        // change the display time
+        document.getElementById('minutes').innerHTML = workMinutes;
+        document.getElementById('seconds').innerHTML = seconds;
+
+        // change the time (start)
+        seconds = seconds - 1;
+    }
+
+    // start countdown
+    setInterval(timerFunction, 1000); // 1000 =  1s;
+
+}
